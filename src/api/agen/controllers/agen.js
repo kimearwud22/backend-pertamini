9e3ab0d74e9c49204a34766ac2cd0bd13bb48f5d
kime arwud <kimearwud22@gmail.com>
2023-01-10 17:03:41 +0700

    first commit

---
+++ src/api/agen/controllers/agen.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * agen controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::agen.agen');
---
+++ src/api/agen/routes/agen.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * agen router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::agen.agen');
